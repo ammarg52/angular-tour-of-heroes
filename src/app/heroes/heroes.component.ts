c45b1f846e6bfe6c4bd83ff28dc3a182c43baf69
778116@cognizant.com <ammar.gangardiwala@cognizant.com>
2023-02-28 17:43:37 -0600

    routing added

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../hero';
 import {HeroService} from '../hero.service';
+import {MessageService} from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -16,7 +17,8 @@ export class HeroesComponent implements OnInit {
 
   heroes: Hero[] = [];
 
-  constructor(private heroService: HeroService) { }
+  constructor(private heroService: HeroService, 
+      private messageService: MessageService) { }
 
   ngOnInit(): void {
     this.getHeroes();
@@ -24,10 +26,12 @@ export class HeroesComponent implements OnInit {
 
   onSelect(hero): void{
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
   getHeroes() : void {
-    this.heroes =  this.heroService.getHeroes();
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
   }
 
 }
